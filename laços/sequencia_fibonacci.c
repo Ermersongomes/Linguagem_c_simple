84223d2692e15fa70b9cab2db97ea0c17baff2c0
Ermersongomes <ermersonwwe@gmail.com>
2024-07-11 11:38:52 -0300

    new update

--- laços/sequencia_fibonacci.c
+++ laços/sequencia_fibonacci.c
@@ -6,5 +6,26 @@
 **/
 
 int main(){
+    //declarações das variáveis
+    float anterior1=0, anterior2=1, proximo;
+
+    //imprimindo os elementos
+    printf("%f ", anterior1);
+    printf("%f ", anterior2);
+
+    //loop de reptição
+    int i, qtd_max=50;
+    for(i=1; i<=qtd_max; i++){
+        proximo = anterior1+anterior2;
+        printf("%f ", proximo);
+
+        //caminhando para frente
+        //atualizando os dois valores
+        anterior1=anterior2;
+        anterior2=proximo;
+        
+
+    }
+    return 0;
     
 }
