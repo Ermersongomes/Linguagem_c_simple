5acf0464b29772a8d28c690dad9f921a53cb9005
Ermersongomes <ermersonwwe@gmail.com>
2024-07-17 20:35:33 -0300

    new update

---
+++ Linguagem_C_Aulas_YouTube/Aula02.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // inclue a biblioteca input e output. std= standar i= input, o=output.
+
+int main() // função principal do programa.
+{
+    printf("Este é meu primeiro código em C!"); // Escreve a mensagem na tela do usuário.
+    return 0;
+}
