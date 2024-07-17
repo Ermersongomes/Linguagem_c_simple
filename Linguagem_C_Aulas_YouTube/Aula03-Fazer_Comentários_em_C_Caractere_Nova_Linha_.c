146697e8956941161151ed2b7fde0390ab9d0a5d
Ermersongomes <ermersonwwe@gmail.com>
2024-07-17 20:58:33 -0300

    new update

---
+++ Linguagem_C_Aulas_YouTube/Aula03-Fazer_Comentários_em_C_Caractere_Nova_Linha_.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // Este é um comentário de linha única
+
+    printf("Usando comentários dentro do código. \n"); // O \n servira para pular um linha no código.
+
+    /* Este é um comentário de múltiplas linhas.
+   Ele pode se estender por várias linhas.
+   Tudo entre ele será ignorado pelo compilador. */
+
+    return 0;
+}
