775eff98bd002035af1920f42dc1501394ace392
Ermersongomes <ermersonwwe@gmail.com>
2024-07-18 16:20:54 -0300

    new update

---
+++ Linguagem_C_Aulas_YouTube/Aula08-Variáveis Tipo float e char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int inteiro = 1000;
+    float decimal = 15.5654;
+    char caracteres = '?';
+
+    printf("%i \n", inteiro);
+    printf("%f \n", decimal);
+    printf("%c \n", caracteres);
+
+    return 0;
+}
