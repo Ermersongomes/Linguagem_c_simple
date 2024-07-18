dc57694b59e77c8b9304f4028786d597222fcd8e
Ermersongomes <ermersonwwe@gmail.com>
2024-07-18 10:07:45 -0300

    new update

--- Vetores/2-vetor.c
+++ Vetores/2-vetor.c
@@ -7,14 +7,36 @@ int main()
     // declaração das variáveis.
     int tamanho = 4; // tamanho do vetor para as notas.
     double notas[tamanho];
+    double soma = 0;
+    double media; 
 
     // Solicitando as notas ao usuário.
     int indice;
     for (indice = 0; indice < 4; indice++)
     {
-        printf("Digite sua nota: %i \n", indice + 1);
-        scanf("%f", &notas[indice]);
+        printf("Digite sua nota %i:  \n", indice + 1);
+        scanf("%lf", &notas[indice]);
+        soma = soma + notas[indice]; //Calculando a soma
     }
+
+    media = soma/tamanho;
+
+    if ( media >= 6)
+    {
+        printf("Aluno aprovado! \n");
+        
+    }
+    else if (media > 4)
+    {
+        printf("Aluno em recuperação! \n");
+    }
+    else
+    {
+        printf("Aluno reprovado! \n");
+    }
+
+    //Informando a média do aluno.
+    printf("Sua média foi: %.2lf\n", media);
 
     return 0;
 }
