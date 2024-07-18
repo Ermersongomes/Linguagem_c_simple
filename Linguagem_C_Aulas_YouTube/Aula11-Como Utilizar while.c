7fbb2cf94f5dbeb61f029dae646211bd810614c6
Ermersongomes <ermersonwwe@gmail.com>
2024-07-18 16:53:59 -0300

    new update

---
+++ Linguagem_C_Aulas_YouTube/Aula11-Como Utilizar while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int cont = 1;
+
+    while (cont <= 5)
+    {
+        printf("Contando... %i \n", cont);
+        cont++;
+    }
+    
+
+    return 0;
+}
