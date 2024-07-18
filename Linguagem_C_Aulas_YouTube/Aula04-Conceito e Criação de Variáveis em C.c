799ba6684930d6c649e31ea1ab38fd81b8d3fc3e
Ermersongomes <ermersonwwe@gmail.com>
2024-07-17 21:14:31 -0300

    new update

---
+++ Linguagem_C_Aulas_YouTube/Aula04-Conceito e Criação de Variáveis em C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int minhaIdade; //int cria variais de números inteiros.
+    minhaIdade = 23; // informa o dado que será guardado dentro da variável
+    printf("Minha idade e %i.\n", minhaIdade); //%i informa que irá usar uma variável inteira.
+
+    return 0;
+}
