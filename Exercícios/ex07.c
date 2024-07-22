e9b8a07d2212792590e3c1a65a0611554860aecc
Ermersongomes <ermersonwwe@gmail.com>
2024-07-22 19:57:24 -0300

    new update

---
+++ Exercícios/ex07.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Produto dos Números de 1 a N
+
+Crie um programa que leia um número inteiro positivo N e calcule o produto
+dos números de 1 até N usando um laço for.*/
+
+int main()
+{
+
+    int n;
+    int produto = 1;
+    int i;
+
+    printf("Digite um numero positivo: \n");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        produto = produto * i;
+
+    }
+
+    printf("O produto de 1  a %d e %d \n", n, produto);
+
+
+
+    return 0;
+}
