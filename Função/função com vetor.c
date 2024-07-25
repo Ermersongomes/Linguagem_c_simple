01bba03ebe1f52a64a8882c3d42ee1665e49849e
Ermersongomes <ermersonwwe@gmail.com>
2024-07-25 10:05:43 -0300

    new update

--- Função/função com vetor.c
+++ Função/função com vetor.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-//função que retorna  a média de 4 números decimais
+// função que retorna  a média de 4 números decimais
 float calcular_media(float n1, float n2, float n3, float n4)
 {
-    return (n1+n2+n3+n4)/4;
+    float media = (n1 + n2 + n3 + n4) / 4;
+    return media;
 }
 
-
-//início do programa
+// início do programa
 int main()
 {
-    //declarações das variáveis
+    // declarações das variáveis
     float media;
+    float n1, n2, n3, n4;
 
-    //executando o programa
-    media = calcular_media(4, 1, 3, 0);
-    
+    n1 = 3;
+    n2 = 5;
+    n3 = 8;
+    n4 = 16;
+
+    // executando o programa
+    media = calcular_media(n1, n2, n3, n4);
+    printf("media: %2.f \n", media);
 
     return 0;
 }
