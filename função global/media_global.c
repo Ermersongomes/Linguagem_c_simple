dfd819fb7b5e80f47ee8d586a9a0b74043cd8e1a
Ermersongomes <ermersonwwe@gmail.com>
2024-07-31 11:15:00 -0300

    new commit

---
+++ função global/media_global.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+float m = 0;
+float pi = 3.14;
+float media = 0;
+//retorna a média entre 3 números
+float calcula_media(float n1, float n2, float n3)
+{
+    m = (n1+n2+n3)/3;
+    return m;
+}
+
+//inicio
+int main()
+{
+    //declaração das variáveis7
+    float n1 = 3, n2 = 5.5, n3 = 2.5;
+    
+    //imprimindo a variável global
+    printf("media: %f -# %p \n", media, &media);
+
+    float media = calcula_media(n1, n2, n3);
+    printf("media: %f -# %p \n", media, &media);
+    
+    //acessando o valor da variável global
+    printf("m: %f \n", m);
+    return 0;
+}
