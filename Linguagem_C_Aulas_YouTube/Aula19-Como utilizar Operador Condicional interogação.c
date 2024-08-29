486e59cdc6c47c6696c840935d5916306b44b837
Ermersongomes <ermersonwwe@gmail.com>
2024-08-29 16:56:22 -0300

    update

--- Linguagem_C_Aulas_YouTube/Aula19-Como utilizar Operador Condicional interogação.c
+++ Linguagem_C_Aulas_YouTube/Aula19-Como utilizar Operador Condicional interogação.c
@@ -5,7 +5,13 @@ int main()
     int num1 = 10;
     int num2 = 20;
 
+    int resposta;
+
     num1 < num2 ? printf("Sim\n") : printf("Nao\n");
+
+    resposta = (num1 < num2) ? 10 : -10;
+
+    printf("%i\n", resposta);
 
     return 0;
 }
